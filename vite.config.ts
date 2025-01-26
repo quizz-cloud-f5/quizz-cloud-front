cbc11b65d21e2987952f665767f1bf7f550872fe
Sergi Virgili <svirgilif@gmail.com>
2025-01-26 11:06:10 +0100

    chore: configure Vitest with global setup and update TypeScript settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+/// <reference types="vitest" />
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // Habilita describe, test, it, expect sin importar
+    environment: "jsdom", // Usa jsdom para simular el navegador
+    setupFiles: "./src/setupTests.ts", // Archivo para configuraciones globales
+  },
 })
