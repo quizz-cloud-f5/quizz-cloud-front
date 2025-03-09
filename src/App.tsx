5e979d6c4753d7c1ee00a74f7124863c20653b24
Sergi Virgili <svirgilif@gmail.com>
2025-03-09 13:24:17 +0100

    feat: add DashboardPage Layout and Overview component with routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./App.css";
 import LoginPage from "./modules/auth/login-page";
 import HomePage from "./modules/home/home-page";
+import DashboardPage from "./modules/dashboard/dashboard-page";
+import Overview from "./modules/overview/overview-page";
 
 
 
@@ -11,6 +13,16 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <DashboardPage />
+          }
+        >
+          <Route index element={<Overview />} />
+          {/* <Route path="documents" element={<DocumentsPage />} />
+          <Route path="chat" element={<Chat />} /> */}
+        </Route>
       </Routes>
     </Router>
   );
--- src/modules/dashboard/dashboard-page.tsx
+++ src/modules/dashboard/dashboard-page.tsx
@@ -13,6 +13,7 @@ import {
     SidebarProvider,
     SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { Outlet } from "react-router-dom"
 
 export default function DashboardPage() {
     return (
@@ -39,12 +40,7 @@ export default function DashboardPage() {
                     </div>
                 </header>
                 <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-                    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                        <div className="aspect-video rounded-xl bg-muted/50" />
-                        <div className="aspect-video rounded-xl bg-muted/50" />
-                        <div className="aspect-video rounded-xl bg-muted/50" />
-                    </div>
-                    <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+                    <Outlet />
                 </div>
             </SidebarInset>
         </SidebarProvider>
