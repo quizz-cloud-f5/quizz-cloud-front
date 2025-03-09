201f10027e68e5f8283b21e9b2d723511e8328b7
Sergi Virgili <svirgilif@gmail.com>
2025-03-09 12:58:30 +0100

    feat: implement LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import LoginPage from "./modules/auth/login-page";
 import DashboardPage from "./modules/dashboard/dashboard-page";
 
 
 function App() {
   return (
-    <DashboardPage />
+    <LoginPage />
   );
 }
 
